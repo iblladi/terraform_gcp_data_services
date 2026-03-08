6a2e0262069fcfbecc092be0f85dc617f5acd3fb
Ismail BLLADI <blladi.ismail@gmail.com>
2026-03-08 17:17:38 +0100

    correct gitlab pipeline

--- terraform_project/modules/bigquery/main.tf
+++ terraform_project/modules/bigquery/main.tf
@@ -3,23 +3,4 @@ resource "google_bigquery_dataset" "dataset" {
   friendly_name               = "${var.dataset_id} dataset"
   description                 = "Dataset for ${var.dataset_id}"
   location                    = var.region
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-
-  access {
-    role          = "roles/bigquery.dataOwner"
-    user_by_email = google_service_account.bqowner.email
-  }
-
-  access {
-    role   = "READER"
-    domain = "hashicorp.com"
-  }
-}
-
-resource "google_service_account" "bqowner" {
-  account_id = "bqowner"
 }
