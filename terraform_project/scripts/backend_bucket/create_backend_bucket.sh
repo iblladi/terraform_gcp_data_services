451aea265a75dee68d1d9175aed608f13584d6bb
Ismail BLLADI <blladi.ismail@gmail.com>
2026-03-08 13:59:14 +0100

    correct backend bucket script

--- terraform_project/scripts/backend_bucket/create_backend_bucket.sh
+++ terraform_project/scripts/backend_bucket/create_backend_bucket.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# bucket name from first argument
 TF_STATE_BUCKET=$1
 
-# fallback to defaults if env vars missing
-REGION=${TF_REGION}
-GCP_PROJECT=${GOOGLE_PROJECT}
-
 echo "TF_STATE_BUCKET=$TF_STATE_BUCKET"
-echo "GCP_PROJECT=$GCP_PROJECT"
-echo "REGION=$REGION"
-
-gcloud auth list
+echo "GCP_PROJECT=$GOOGLE_PROJECT"
+echo "REGION=$TF_REGION"
 
 echo "Checking if bucket exists..."
-if gsutil ls -b gs://$TF_STATE_BUCKET >/dev/null 2>&1; then
-  echo "Bucket already exists"
-else
+
+gcloud storage buckets describe gs://${TF_STATE_BUCKET} >/dev/null 2>&1
+
+if [ $? -ne 0 ]; then
   echo "Creating Terraform backend bucket..."
-  gsutil mb -p $GCP_PROJECT -l $REGION gs://$TF_STATE_BUCKET
-  gsutil versioning set on gs://$TF_STATE_BUCKET
+
+  gcloud storage buckets create gs://${TF_STATE_BUCKET} \
+      --project=${GOOGLE_PROJECT} \
+      --location=${TF_REGION}
+
+  echo "Enabling versioning..."
+
+  gcloud storage buckets update gs://${TF_STATE_BUCKET} \
+      --versioning
+else
+  echo "Bucket already exists."
 fi
